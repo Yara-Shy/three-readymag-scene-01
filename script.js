9db5a140c07a4ec7361f565a5a2f72fd048f14e9
Yara-Shy <yara.shylyk.s@gmail.com>
2026-02-06 13:42:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const split = document.querySelector(".split");
+
+// приймаємо повідомлення
+window.addEventListener("message", (event) => {
+  if (!event.data.scrollProgress) return;
+
+  const progress = event.data.scrollProgress;
+  const maxHeight = window.innerHeight;
+
+  split.style.height = progress * maxHeight + "px";
+});
 
